144521891f093b36e615b1f01c7fe633d262f882
MorningM <2428003515@qq.com>
2026-07-12 19:17:20 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        // 左闭右开区间：the interval is closed on the left and open on the right
+        let left = 0, right = nums.length;
+        while(left < right){
+            const mid = Math.floor((left + right) / 2);
+            if(nums[mid] === target){
+                return mid;
+            }else if(nums[mid] < target){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
+        }
+        return -1
+    }
+}
