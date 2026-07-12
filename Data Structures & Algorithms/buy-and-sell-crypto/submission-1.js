fc12e7516617a04ff1dd4aef62dfdf7831bf571b
MorningM <2428003515@qq.com>
2026-07-12 13:57:50 -0400

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        // dp: everyday, the maxProfit is max or prices[i] - minPrice (the max from yesterday or today - min)
+        let minPrice = Infinity;
+        let maxProfit = 0;
+        for(let price of prices){
+            minPrice = Math.min(price, minPrice);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit
+        
+    }
+}
