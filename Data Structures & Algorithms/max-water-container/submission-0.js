a0377d7f382d3f2d81520693d7020c5c34d9963d
MorningM <2428003515@qq.com>
2026-07-12 13:40:43 -0400

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        // the amount of water depends on the shorter bar, so move the shorter side
+        let left = 0, right = heights.length - 1;
+        let max = 0;
+        while(left < right){
+            const amount = Math.min(heights[left], heights[right]) * (right - left);
+            max = Math.max(amount, max);
+            if(heights[left] < heights[right]){
+                left++;
+            }else{
+                right--
+            }
+
+        }
+        return max;
+        
+    }
+}
